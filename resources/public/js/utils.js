254b972dbca47224e0e45ab1bbc1d41b9b39bb9d
Ryan Kohl <ryanckohl@gmail.com>
2013-02-10 13:30:11 -0500

    moving around directory structure

---
+++ resources/public/js/utils.js
@@ -0,0 +1,23 @@
+function intersect_all(lists) {
+	if (lists.length == 0) return [];
+	else if (lists.length == 1) return lists[0];
+	var partialInt = lists[0];
+	for (var i = 1; i < lists.length; i++) {
+		partialInt = intersection(partialInt, lists[i]);
+	}
+	return partialInt;
+}
+function intersection(a, b) {
+	var ai=0, bi=0;
+	var result = new Array();
+	while( ai < a.length && bi < b.length ) {
+		if      (a[ai] < b[bi] ){ ai++; }
+		else if (a[ai] > b[bi] ){ bi++; }
+		else /* they're equal */ {
+			result.push(a[ai]);
+			ai++;
+			bi++;
+		}
+	}
+	return result;
+}
